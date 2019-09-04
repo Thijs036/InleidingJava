ea7ec4d6f3ee81bccfba779c530238e325376afc
Thijs <t.oldenburger@hotmail.nl>
2019-09-04 11:20:27 +0200

    Opdracht 2.2

---
+++ src/VragenEnOpdracht2.java
@@ -0,0 +1,31 @@
+import java.applet.*;
+import java.awt.*;
+
+public class VragenEnOpdracht2 extends Applet {
+
+    public void init() {
+        setBackground(Color.BLUE);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.drawString("Thijs", 50, 60);
+        /*
+        Wat is machine taal?
+        Machinetaal is een taal waarin instructies geschreven zijn die de processor van een computer direct kan uitvoeren
+
+        Welke drie hulpmiddelen heb je nodig om te programmeren?
+        een programmeertaal, een applet en een code editor
+
+        Hoe herken je opdrachten in de broncode?
+        Alle opdrachten zitten tussen de accolades.
+
+        Waartoe dienen de import-opdrachten?
+        Die halen de voorgedifinieerde codes op uit de klassenbibliotheek
+
+        Wat is een methode?
+        een methode is een aantal opdrachten met een naam erboven.
+         */
+    }
+}
+
