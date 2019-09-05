caae443da746a9b42779bb07668c98f0fbde801f
Thijs <t.oldenburger@hotmail.nl>
2019-09-05 13:49:51 +0200

    Opdracht 4.33

--- src/H04/H4Opdracht3.java
+++ src/H04/H4Opdracht3.java
@@ -1,7 +1,9 @@
+package H04;
+
 import java.applet.Applet;
 import java.awt.*;
 
-public class Opdracht3 extends Applet {
+public class H4Opdracht3 extends Applet {
 
 
     public void init() {
@@ -9,10 +11,10 @@ public class Opdracht3 extends Applet {
 
     public void paint(Graphics g) {
         g.setColor(Color.yellow);
-        g.fillOval(5,50,50,50);
-        g.drawOval(5, 50, 50, 50);
+        g.fillOval(1,85,50,50);
+        g.drawOval(1, 85, 50, 50);
         g.setColor(Color.BLACK);
-        g.drawLine(30,400,30, 100);
+        g.drawRect(10, 135, 20, 400);
         g.drawRect(30,150,150,46);
         g.drawRect(30,150,150,100);
         g.drawRect(30,150,150,150);
