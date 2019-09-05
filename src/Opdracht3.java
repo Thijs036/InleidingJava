c9245315e2ea38496d624240f9b7f49ca623aa4b
Thijs <t.oldenburger@hotmail.nl>
2019-09-05 10:05:39 +0200

    Opdracht 4.33

--- src/Opdracht3.java
+++ src/Opdracht3.java
@@ -8,6 +8,10 @@ public class Opdracht3 extends Applet {
     }
 
     public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillOval(5,50,50,50);
+        g.drawOval(5, 50, 50, 50);
+        g.setColor(Color.BLACK);
         g.drawLine(30,400,30, 100);
         g.drawRect(30,150,150,46);
         g.drawRect(30,150,150,100);
@@ -16,5 +20,6 @@ public class Opdracht3 extends Applet {
         g.fillRect(30,150,150,50);
         g.setColor(Color.blue);
         g.fillRect(30,250,150,50);
+
     }
 }
