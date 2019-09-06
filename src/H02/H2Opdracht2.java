527271efaab63d17f3f06b386c2d19c0d50fd973
Thijs <t.oldenburger@hotmail.nl>
2019-09-06 10:53:36 +0200

    Opdracht 4.33

---
+++ src/H02/H2Opdracht2.java
@@ -0,0 +1,17 @@
+package H02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class H2Opdracht2 extends Applet {
+    public void init()  {
+
+    }
+        public void paint(Graphics g) {
+            g.setColor(Color.BLUE);
+            g.drawString("Thijs", 50, 60);
+            g.setColor(Color.RED);
+            g.drawString("Oldenburger", 50, 70);
+    }
+}
